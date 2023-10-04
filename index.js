9e2847bcdc9addd97151aff1031e4ac45bb2675c
niteshim0 <niteshim111@gmail.com>
2023-10-04 13:49:18 +0530

    server side rendering

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
+const path = require("path");
 const urlRoute  = require("./routes/url");
 const { connectToMongoDB } = require("./connection");
+const staticRouter = require("./routes/staticRouter");
 const URL = require("./models/url");
 
 const app = express();
@@ -10,9 +11,12 @@ const PORT = 8001;
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url')
 .then(()=>{console.log('MongoDB connected!')});
 
-app.use(express.json());//middleware to parse the incoming bodies
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));//middleware to parse the incoming bodies
+app.set("view engine","ejs");//middleware for setting the view engine 
+app.set("views",path.resolve("./views"))//path of views
 
-app.get("/:shortId",async(req,res)=>{
+app.get("/url/:shortId",async(req,res)=>{
   const shortId = req.params.shortId;
   const entry = await URL.findOneAndUpdate(
     {
@@ -27,6 +31,7 @@ app.get("/:shortId",async(req,res)=>{
     }
   );
   return res.redirect(entry.redirectURL);
-})
+});
+app.use("/",staticRouter);
 app.use("/url",urlRoute);
 app.listen(PORT,()=> console.log(`Server started at PORT : ${PORT}`));
